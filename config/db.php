bc648c8e8dd3ee1a24ef38aa798919cb7c42efeb
exclamatory357 <107873673+exclamatory357@users.noreply.github.com>
2024-07-19 13:58:44 +0800

    commit

--- config/db.php
+++ config/db.php
@@ -2,16 +2,26 @@
 
 error_reporting(0);
 
-$host = "sql109.infinityfree.com"; // The hostname from your MySQL details on InfinityFree
-$username = "if0_36823419"; // Your MySQL username on InfinityFree
-$password = "nexus3572"; // Your MySQL password on InfinityFree
-$db = "if0_36823419_resevation"; // The full database name on InfinityFree
+$host = "localhost";
 
-$con = mysqli_connect($host, $username, $password, $db);
+$username = "root";
+
+$password ="";
+
+$db = "resevation";
+
+$con = mysqli_connect($host,$username,$password,$db);
+
+
 
 // Check connection
+
 if (!$con) {
+
     die("Connection failed: " . mysqli_connect_error());
+
 }
-// echo "Connected successfully";
+
+//echo "Connected successfully";
+
 ?>
