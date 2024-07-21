049354b7d2e8633b1155d0c261fe5a55f1178ecd
exclamatory357 <107873673+exclamatory357@users.noreply.github.com>
2024-07-21 14:06:57 +0800

    commit

--- home/view/home.php
+++ home/view/home.php
@@ -22,7 +22,7 @@
         .p-absolute-login {
             position: absolute;
             top: 50%;
-            left: 50%;
+            left: 30%;
             transform: translate(-50%, -50%);
             width: 100%;
             max-width: 400px; /* Adjust width as needed */
@@ -34,6 +34,7 @@
             width: 100%;
             text-align: center;
             color: white;
+            
         }
         
         .carousel-caption {
