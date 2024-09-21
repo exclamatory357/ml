4cc32868beac465b1bc77cbb1b8aabf0dc5d1490
exclamatory357 <107873673+exclamatory357@users.noreply.github.com>
2024-09-22 05:23:19 +0800

    commit

--- home/view/gallery.php
+++ home/view/gallery.php
@@ -35,5 +35,24 @@ if (isset($_GET["gallery"])) {?>
         </div>
 	</div>
 </div>
+   <!-- LOGIN PAGE -->
+   <?php if (!isset($_SESSION["username"])) { ?>
+            <!-- LOGIN FORM, show if session is not set -->
+            <div class="login-box-body p-absolute-login container mt-5">
+                <p class="login-box-msg text-center">Welcome back!</p>
+                <form action="function/login.php" method="post">
+                    <div class="form-group has-feedback">
+                        <input type="text" class="form-control form-control-lg" placeholder="Enter Username" name="username" required autofocus>
+                        <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                    </div>
+                    <div class="form-group has-feedback">
+                        <input type="password" class="form-control form-control-lg" placeholder="Enter Password" name="password" required>
+                        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-block btn-lg" name="btnlogin">Sign In</button>
+                    <!-- <button type="button" data-toggle="modal" data-target="#modal-registration" class="btn btn-success btn-block btn-lg">Create Account</button> -->
+                </form>
+            </div>
+        <?php } ?>
     </section>
 <?php }?>
