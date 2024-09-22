4ef27b33961b15319bfdadbc7c600579de28f976
exclamatory357 <107873673+exclamatory357@users.noreply.github.com>
2024-09-22 22:16:53 +0800

    commit

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
  //header("location: home?home");
- header("location: https://www.pornhub.com");
+ header("location: https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBbRsdDTzPCkSfAit9JSunGyCdQ2t-nlFsqA&s");
 ?>
