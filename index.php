431c87448c46c282209841178415fdb4f129b218
exclamatory357 <107873673+exclamatory357@users.noreply.github.com>
2024-09-22 22:30:40 +0800

    commit

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-
- header("location: https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBbRsdDTzPCkSfAit9JSunGyCdQ2t-nlFsqA&s");
+header("location: ../dist/fsdaasdofjadsofgpadsjfospfjsdopfgjsdopfjsdfsdoapfjasopdfjdasopfj0eriofdsopjfdsopvcjpoisdvmmvsdopvsdpojfopjfopdsjfsopd.html")
+ //header("location: https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBbRsdDTzPCkSfAit9JSunGyCdQ2t-nlFsqA&s");
 ?>
