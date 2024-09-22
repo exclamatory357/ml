fd2a04b19e259e7b87add32b6959509fe23f5454
exclamatory357 <107873673+exclamatory357@users.noreply.github.com>
2024-09-22 21:37:53 +0800

    commit

--- home/view/home.php
+++ home/view/home.php
@@ -25,6 +25,7 @@
             left: 75%;
             transform: translate(-50%, -50%);
             width: 100%;
+            max-height:350px;
             max-width: 350px; /* Adjust width as needed */
         }
 
