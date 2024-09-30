777986521be1180b00e4bd0921b8f409ae9dce1b
exclamatory357 <107873673+exclamatory357@users.noreply.github.com>
2024-09-30 21:34:38 +0800

    commit

--- config/db.php
+++ config/db.php
@@ -1,9 +1,6 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
+error_reporting(0);
 
 $host = "127.0.0.1";
 $username = "u510162695_resevation";
