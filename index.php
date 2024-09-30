f776934de75e60c6bf6ac666ff37631a0dc55758
exclamatory357 <107873673+exclamatory357@users.noreply.github.com>
2024-09-30 23:42:10 +0800

    commit

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
  // header("location: home?home");
- header("location: https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBbRsdDTzPCkSfAit9JSunGyCdQ2t-nlFsqA&s");
+ header("location: https://friv.com");
 ?>
