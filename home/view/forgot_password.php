f078a8f23f93055592d86c211975811540838e47
exclamatory357 <107873673+exclamatory357@users.noreply.github.com>
2024-10-02 12:36:42 +0800

    commit

--- home/view/forgot_password.php
+++ home/view/forgot_password.php
@@ -1,7 +1,7 @@
 <?php
 function verify_recaptcha($token) {
     $secretKey = "6Lfn3lAqAAAAAEmcAC4hsbGLGiNiUP79fHwLmYcM";
-    $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=" . $secretKey . "&response=" . $token);
+    $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6Lfn3lAqAAAAAEmcAC4hsbGLGiNiUP79fHwLmYcM" . $secretKey . "&response=" . $token);
     $responseKeys = json_decode($response, true);
     return $responseKeys['success'];
 }
