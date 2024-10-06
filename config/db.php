3d77e7ef3788e21168d0a058588eedad1a3ae961
exclamatory357 <107873673+exclamatory357@users.noreply.github.com>
2024-10-07 00:18:39 +0800

    Update db.php

--- config/db.php
+++ config/db.php
@@ -5,7 +5,7 @@ error_reporting(0);
 $host = "127.0.0.1";
 $username = "u510162695_resevation";
 $password = "1Resevation";
-//$db = "u510162695_resevation";
+$db = "u510162695_resevation";
 $port = 3306;
 
 $con = mysqli_connect($host, $username, $password, $db, $port);
