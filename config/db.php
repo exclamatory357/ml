612ea9597b80620c7760910b986907a54c4315fb
exclamatory357 <107873673+exclamatory357@users.noreply.github.com>
2024-10-10 22:29:13 +0800

    commit

--- config/db.php
+++ config/db.php
@@ -2,24 +2,17 @@
 
 error_reporting(0);
 
-$host = "localhost";
+$host = "127.0.0.1";
+$username = "u510162695_resevation";
+$password = "1Resevation";
+$db = "u510162695_resevation";
+$port = 3306;
 
-$username = "root";
-
-$password ="";
-
-$db = "resevation";
-
-$con = mysqli_connect($host,$username,$password,$db);
-
-
+$con = mysqli_connect($host, $username, $password, $db, $port);
 
 // Check connection
-
 if (!$con) {
-
     die("Connection failed: " . mysqli_connect_error());
-
 }
 
 //echo "Connected successfully";
