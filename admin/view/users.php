0ae989113f09f8fc8e01916ca32ea071c030ca61
exclamatory357 <107873673+exclamatory357@users.noreply.github.com>
2024-11-10 23:07:15 +0800

    kwe

--- admin/view/users.php
+++ admin/view/users.php
@@ -171,11 +171,11 @@ if (isset($_GET["users-edit"])) { ?>
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-6">
+                <h1><a href="?users">Back</a></h1>
                     <h1>Edit User</h1>
-                    h1><a href="?users">Back</a></h1>
                 </div>
                 <div class="col-6">
-                <
+                
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="?users"><i class="fa fa-dashboard"></i> Home</a></li>
                         <li class="breadcrumb-item active">Edit User</li>
