a70f3ad3bbb8e61bf38fb565a82322d73390209e
exclamatory357 <107873673+exclamatory357@users.noreply.github.com>
2024-12-01 23:23:31 +0800

    k

--- home/view/home.php
+++ home/view/home.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 <script src="https://www.google.com/recaptcha/api.js?render=6LdDXo8qAAAAAH1-5iAjDN6HEDN17Tly-GwxcrjZ"></script>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
