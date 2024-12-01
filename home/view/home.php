3a141a6b6f659ce3c8f79793af89ff82fc896509
exclamatory357 <107873673+exclamatory357@users.noreply.github.com>
2024-12-02 02:48:22 +0800

    bolik

--- home/view/home.php
+++ home/view/home.php
@@ -76,8 +76,7 @@
                 width: 95%; /* Set specific width for small mobile screens */
                 max-width: 95%;
                 padding: 10px;
-                opacity: 250%;
-                background: rgba(255, 255, 255, 0.7); /* Slightly more opaque for readability on mobile */
+                background: rgba(255, 255, 255, 0.9); /* Slightly more opaque for readability on mobile */
             }
 
             .login-box-body {
