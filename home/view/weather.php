ddf22f51d6decfe58b38f89909341f5b35c260d0
exclamatory357 <107873673+exclamatory357@users.noreply.github.com>
2024-12-17 12:40:11 +0800

    ha

--- home/view/weather.php
+++ home/view/weather.php
@@ -1,5 +1,11 @@
-<?php
-if (isset($_GET["weather"])) {
-    echo '<iframe src="https://bantayan-bfp.com" width="600" height="400" frameborder="0"></iframe>';
-}
-?>
+<div class="weatherwidget-io" data-label_1="BANTAYAN" data-label_2="WEATHER" data-theme="original" data-mode="Current" data-font="Sans-serif"></div>
+<script>
+    !function(d,s,id){
+        var js,fjs=d.getElementsByTagName(s)[0];
+        if(!d.getElementById(id)){
+            js=d.createElement(s);js.id=id;
+            js.src='https://weatherwidget.io/js/widget.min.js';
+            fjs.parentNode.insertBefore(js,fjs);
+        }
+    }(document, 'script', 'weatherwidget-io-js');
+</script>
