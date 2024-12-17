7b31aa227db244610d9af5d84b95d376d668ef1a
exclamatory357 <107873673+exclamatory357@users.noreply.github.com>
2024-12-17 12:37:08 +0800

    haha

--- home/view/weather.php
+++ home/view/weather.php
@@ -1,5 +1,5 @@
 <?php
 if (isset($_GET["weather"])) {
-    echo '<iframe src="https://www.msn.com/en-xl/weather" width="600" height="400" frameborder="0"></iframe>';
+    echo '<iframe src="https://bantayan-bfp.com" width="600" height="400" frameborder="0"></iframe>';
 }
 ?>
