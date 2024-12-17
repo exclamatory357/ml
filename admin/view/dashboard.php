b86970074d879867a6e10945504636b6a77d8e86
exclamatory357 <107873673+exclamatory357@users.noreply.github.com>
2024-12-17 23:07:19 +0800

    a

--- admin/view/dashboard.php
+++ admin/view/dashboard.php
@@ -578,7 +578,7 @@ body {
             datasets: [
                 {
                     label: 'Fish Catches',
-                    data: <?= json_encode($catch_quantities) ?>, // Quantities caught
+                    data: <?= json_encode($catch_item) ?>, // Quantities caught
                     backgroundColor: 'rgba(75, 192, 192, 0.2)',
                     borderColor: 'rgba(75, 192, 192, 1)',
                     borderWidth: 1
